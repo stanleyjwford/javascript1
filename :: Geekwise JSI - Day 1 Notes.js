8daf6a1be2ad527b763aa4fcd36fe96b7fd681f9
Stan Ford <stanford@Stans-MacBook-Pro.local>
2016-02-24 19:03:40 -0800

    first commit

---
+++ :: Geekwise JSI - Day 1 Notes.js
@@ -0,0 +1,31 @@
+// Geekwise JSI - Day 1 Notes
+// Stan Ford
+// 2/22/2016
+"use strict";
+
+// alert("Hello World");
+
+var favFood =  'Burrito';
+var favDrink = "Root beer";
+
+var favFoodAndDrink = favFood + favDrink;
+
+console.log(favFoodAndDrink);
+
+var firstNumber = 1;
+var secondNumber = 2;
+var thirdNumber = 3;
+
+var sum = firstNumber + secondNumber + thirdNumber;
+var average = sum / 3;
+
+
+console.log(sum);
+console.log(average);
+
+var numArray = [27, 15, 42];
+
+
+
+
+
---
+++ app01.js
@@ -0,0 +1,19 @@
+//stan ford
+//2/22/16
+//Javascript 1
+
+var num1 = 12;
+//now a hexadecimal number 30
+var num2 = 0x1e;
+var string = "Answer equals:";
+
+var sum = num1 + num2;
+
+var result = string + sum;
+
+console.log (result);
+
+//var Answer = //concatenate the string from your third variale with the number from your fourth (new0 vaairable.  Save this to a new variable called answer
+
+//copy your whole code into the develeper console and hit <enter>
+//view the contents of variable "answer"
